87ea4cba8e9b74560aa2466d4bc32bac3d3d7e01
Lav-i <lan_xiao163@163.com>
2017-06-26 16:54:32 +0800

    add description

--- P1/Fibonacci.java
+++ P1/Fibonacci.java
@@ -5,15 +5,15 @@ import java.math.BigInteger;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		BigInteger fibo[] = new BigInteger[100];
-		fibo[0] = fibo[1] = BigInteger.ONE;
+		BigInteger fibo[] = new BigInteger[100];//新建数组存储数列的前一百项
+		fibo[0] = fibo[1] = BigInteger.ONE;//初始化第一项与第二项
 
 		for(int i = 2; i < 100; i++){
-			fibo[i]=fibo[i-1].add(fibo[i-2]);
+			fibo[i]=fibo[i-1].add(fibo[i-2]);//利用循环给每一项赋值
 		}
 
 		for(int j = 0; j < 100; j++){
-			System.out.println(fibo[j]);
+			System.out.println(fibo[j]);//输出
 		}
 
 	}
